1174cf563f7785dd2664ea8ff3c1be55d1d49537
amir <cavalier7654@gmail.com>
2024-08-02 21:12:00 +0330

    update todolist

--- src/components/TodList/List.jsx
+++ src/components/TodList/List.jsx
@@ -1,11 +1,23 @@
 import { CiCircleCheck, CiCircleRemove } from "react-icons/ci";
 
-export default function List() {
+export default function List({ title, setTodo , todo , id}) {
 	return (
-		<div className="w-80 h-10 flex bg-violet-400 justify-between items-center pl-4">
-			<p className="text-xl">list</p>
+		<div
+			className={`w-80 h-10 flex ${
+				todo[id - 1].isCompleted ? "bg-violet-500" : "bg-violet-400"
+			}  justify-between items-center pl-4 shadow-xl shadow-black/50 hover:-translate-y-2 transition-all duration-500`}
+		>
+			<p className="text-xl">{title}</p>
 			<div className="flex">
-				<button className="size-10 text-xl flex justify-center items-center bg-teal-600 hover:bg-teal-800 transition-all duration-300 text-white">
+				<button
+					onClick={() => {
+                        const change = [...todo];
+                        change[id - 1].isCompleted = true;
+                        setTodo(change); 
+						console.log(change[id - 1]);
+					}}
+					className="size-10 text-xl flex justify-center items-center bg-teal-600 hover:bg-teal-800 transition-all duration-300 text-white"
+				>
 					<CiCircleCheck />
 				</button>
 				<button className="size-10 text-xl flex justify-center items-center bg-red-600 hover:bg-red-800 transition-all duration-300 text-white">
--- src/components/TodList/TodoList.jsx
+++ src/components/TodList/TodoList.jsx
@@ -1,34 +1,76 @@
+import { useState } from "react";
 import Btn from "./Btn";
 import List from "./List";
+import { CiCircleRemove } from "react-icons/ci";
 
 export default function TodoList() {
+	const [todo, setTodo] = useState([]);
+	const [showInp, setShowInp] = useState(false);
+
 	return (
 		<div className="bg-slate-300 min-h-[100vh] flex justify-start items-center gap-4 flex-col relative ">
-			<div className="fixed top-[30%]  bg-slate-400 shadow-md shadow-black p-10 rounded-2xl ">
-				<form
-					action=""
-					className="flex flex-col justify-center items-center w-full gap-20"
-				>
-					<div className="flex flex-col justify-center items-center gap-5">
-						<label htmlFor="" className="font-semibold">add your job</label>
-						<input
-							type="text"
-							name=""
-							id=""
-							className=" w-[320px] outline-none focus-visible:ring-4 ring-slate-500 rounded-md px-4 py-1 shadow-lg shadow-black"
-						/>
+			{showInp && (
+				<div className="w-[100vw] h-screen bg-black/30 text-center fixed flex justify-center items-center">
+					<div className=" bg-slate-400 shadow-md te shadow-black sm:p-10 py-10 px-4 rounded-2xl ">
+						<div className="flex justify-end">
+							<button
+								className="shadow rounded-full shadow-black mb-7"
+								onClick={() => {
+									setShowInp(false);
+								}}
+							>
+								<CiCircleRemove className="text-xl" />
+							</button>
+						</div>
+						<form
+							onSubmit={(e) => {
+								e.preventDefault();
+								const newTodo = [...todo];
+
+								newTodo.push({
+									title: e.target[0].value,
+									isCompleted: false,
+									id: newTodo.length != 0
+										? newTodo[newTodo.length - 1].id + 1
+										: 1,
+								});
+								setTodo(newTodo);
+								e.target[0].value = "";
+								setShowInp(false);
+							}}
+							action=""
+							className="flex flex-col justify-center items-center w-full gap-20"
+						>
+							<input
+								placeholder="add your job"
+								type="text"
+								name=""
+								id=""
+								className="w-[320px] outline-none focus-visible:ring-4 ring-slate-500 rounded-md px-4 py-1 shadow-lg shadow-black"
+							/>
+
+							<button className="size-full bg-slate-800 text-white rounded-md w-32 h-10 font-semibold shadow-md shadow-black">
+								add
+							</button>
+						</form>
 					</div>
-					<Btn>add</Btn>
-				</form>
-			</div>
+				</div>
+			)}
 			<div className="container max-w-maxWidth mt-28 flex justify-between items-center z">
-				<Btn>add</Btn>
+				<Btn click={() => setShowInp(true)}>add</Btn>
 
 				<select className="size-full bg-slate-800 rounded-md w-32 h-10 text-center  shadow-md shadow-black text-white font-semibold">
 					<option value="all">all</option>
 					<option value="complate">complate</option>
 					<option value="unComplate">unComplate</option>
 				</select>
+			</div>
+			<div className="mt-14 grid grid-cols-1 md:grid-cols-2 gap-7 container lg:grid-cols-3 justify-center items-center justify-items-center max-w-maxWidth">
+				{todo.length > 0 &&
+					todo.map(
+						(t) => <List title={t.title} key={t.id} setTodo={setTodo} todo={todo} id={t.id} />
+					
+					)}
 			</div>
 		</div>
 	);
